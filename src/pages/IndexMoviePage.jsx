149e010148ca5e403c4a0d34b799125f5d22f293
GalloSimone <gallosimonecz@gmail.com>
2025-01-13 18:02:04 +0100

    milestone 3

--- src/pages/IndexMoviePage.jsx
+++ src/pages/IndexMoviePage.jsx
@@ -1,7 +1,58 @@
-export default function IndexMoviePage(){
-    return(
-        <>
-        <h1>movie list</h1>
-        </>
-    )
-} 
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function HomePage() {
+  const [movies, setMovies] = useState([]); // State per il salvataggio dei film
+
+  useEffect(() => {
+    // Fetch all'API Express
+    fetch("http://localhost:3000/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data);
+      });
+  }, []);
+
+  return(
+
+<div className="container">
+    <h1>lista post </h1>
+
+    <ul>
+    {movies.map((movie)=>(
+        <li key={movie.id}>
+            {movie.title}
+        </li>
+    ))}
+    </ul>
+    
+    </div>
+    
+  )
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
