835db68cea2a8fff9913e838b6c86452b03cb169
GalloSimone <gallosimonecz@gmail.com>
2025-01-13 16:22:54 +0100

    modify navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
 import {BrowserRouter, Route, RouterProvider, Routes} from "react-router-dom"
 import HomePage from './pages/HomePage'
-import About from './pages/AboutPage'
 import AboutPage from './pages/AboutPage'
+import DefaultLayout from './layouts/DefaultLayout'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <BrowserRouter>
    <Routes>
-    <Route index element={<HomePage/>}></Route>
-    <Route path='/about' element={<AboutPage/>}></Route>
+     <Route element={<DefaultLayout/>}path='/'> 
+     <Route index element={<HomePage/>}></Route>
+     <Route path='/about' element={<AboutPage/>}></Route>
+   </Route>
    </Routes>
    </BrowserRouter>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 export default function HomePage(){
    return(
+    <div className="container">
     <h1>homepage</h1>
-   ) 
+    </div>
+)
 }
