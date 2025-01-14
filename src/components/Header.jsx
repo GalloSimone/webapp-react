8d6eb5b019328277fb3a6ad741b6e6ded42f678e
GalloSimone <gallosimonecz@gmail.com>
2025-01-14 15:57:52 +0100

    add button to return index movies

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,18 +5,12 @@ return(
     <>
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <NavLink className="navbar-brand" to="/">movie-react</NavLink>
+    <NavLink className="navbar-brand" to="/movies">movie-react</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/about">About</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/movies">movies </Link>
-        </li>
       </ul>
     </div>
   </div>
--- src/pages/DetailMoviePage.jsx
+++ src/pages/DetailMoviePage.jsx
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 
 
 export default function DetailMoviePage(){
@@ -18,40 +18,30 @@ useEffect(() => {
       });
   }, []);
   return (
-    <div className="card m-5 p-3" style={{width: "18rem"}}>
+
+    <div className=" card m-5 p-3" style={{width: "18rem"}}>
+       
   <div className="card-body">
-    {movie && <h5 className="card-title">{movie.title}</h5>}
-    {movie && <h6 className="card-subtitle mb-2 text-body-secondary">Regista: <strong>{movie.director}</strong></h6>}
-    { movie && <p className="card-text"> Genere: <strong>{movie.genre}</strong></p>}
-    {movie && <p  className="card-text">Anno di pubblicazione: <strong>{movie.release_year}</strong></p>}
+    
+    {movie && <h3 className="card-title">{movie.title}</h3>}
+    {movie && <h6 className="card-text">Regista: <strong>{movie.director}</strong></h6>}
+    { movie && <h6 className="card-text"> Genere: <strong>{movie.genre}</strong></h6>}
+    {movie && <h6  className="card-text">Anno di pubblicazione: <strong>{movie.release_year}</strong></h6>}
     <h2>Recensioni</h2>
     {reviews.length > 0 ? (
         reviews.map((review, index) => 
         <p key={index}>
-            <strong>{review.name}</strong>:{review.text}<br/>
-            Rating:<strong>{review.vote}</strong></p> )
+            <strong>{review.name}</strong>: {review.text}<br/>
+            Rating: <strong>{review.vote}</strong></p> )
       ) : (
         <p>No reviews available</p>
       )}
-
-
   </div>
+  <Link to={'/movies'} >
+        <button className='btn btn-primary w-100'>
+            Ritorna alla lista film 
+        </button>
+        </Link>
 </div>
-
-
-
-
-
-    
-
-      /* <h2>Recensioni</h2>
-      {reviews.length > 0 ? (
-        reviews.map((review, index) => 
-        <p key={index}>
-            <strong>{review.name}</strong>:{review.text}<br/>Rating:{review.vote}</p> )
-      ) : (
-        <p>No reviews available</p>
-      )}
-    </div>*/
   );
 }
